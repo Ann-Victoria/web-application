86ce91fb27d43a51b53ee2b4f2fd4fb68174adad
Victoria <wwaruri@gmail.com>
2021-07-19 06:53:12 +0300

    add onclick event

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,9 @@
-var gender = prompt('choose gender')
+let myGender = document.querySelector('html')
+myGender.onclick =function() {
+  prompt('choose your gender')
+}
 function validation()
-if(0>=day>31 && 0>=month>12) {
+if(((0<=day>31) && (0<=month>12))) {
   alert('invalid')
 } else {
   alert('valid')
@@ -22,31 +25,31 @@ let dayOfTheWeek = parseInt(result)
 var akanFemaleName =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 var akanMaleName =['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame']
 function akanName() {
-if(gender===female && dayOfTheWeek===sunday) {
+if(myGender===female && dayOfTheWeek===sunday) {
   alert('Your Akan name is ' + akanFemaleName['0'])
-} else if(gender===female && dayOfTheWeek===monday) {
+} else if(myGender===female && dayOfTheWeek===monday) {
   alert('Your Akan name is ' + akanFemaleName['1'])
-} else if(gender===female && dayOfTheWeek===tuesday) {
+} else if(myGender===female && dayOfTheWeek===tuesday) {
   alert('Your Akan name is ' + akanFemaleName['2'])
-} else if(gender===female && dayOfTheWeek===wednesday) {
+} else if(myGender===female && dayOfTheWeek===wednesday) {
   alert('Your Akan name is ' + akanFemaleName['3'])
-} else if(gender===female && dayOfTheWeek===thursday) {
+} else if(myGender===female && dayOfTheWeek===thursday) {
   alert('Your Akan name is '+ akanFemaleName['4'])
-} else if(gender===female && dayOfTheWeek===friday) {
+} else if(myGender===female && dayOfTheWeek===friday) {
   alert('Your Akan name is ' + akanFemaleName['5'])
-} else if(gender===female && dayOfTheWeek===saturday) {
+} else if(myGender===female && dayOfTheWeek===saturday) {
   alert('Your Akan name is '+ akanFemaleName['6'])
-} else if(gender===male && dayOfTheWeek===sunday) {
+} else if(myGender===male && dayOfTheWeek===sunday) {
   alert('Your Akan name is ' + akanMaleName['0'])
-} else if(gender===male && dayOfTheWeek===monday) {
+} else if(myGender===male && dayOfTheWeek===monday) {
   alert('Your Akan name is '+ akanMaleName['1'])
-} else if (gender===male && dayOfTheWeek===tuesday) {
+} else if(myGender===male && dayOfTheWeek===tuesday) {
   alert('Your Akan name is '+ akanMaleName['2'])
-} else if(gender===male && dayOfTheWeek===wednesday) {
+} else if(myGender===male && dayOfTheWeek===wednesday) {
   alert('Your Akan name is '+ akanMaleName['3'])
-} else if(gender===male && dayOfTheWeek===thursday) {
+} else if(myGender===male && dayOfTheWeek===thursday) {
   alert('Your Akan name is '+ akanMaleName['4'])
-} else if(gender===male && dayOfTheWeek===friday) {
+} else if(myGender===male && dayOfTheWeek===friday) {
   alert('Your Akan name is '+ akanMaleName['5'])
 } else{
   alert('Your Akan name is '+ akanMaleName['6'])
