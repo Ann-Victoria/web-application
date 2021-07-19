1c4fb38df77550465b988bd75b71781f12b04e74
Victoria <wwaruri@gmail.com>
2021-07-19 03:32:16 +0300

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+let myButton = document.querySelector('button');
+myButton.onclick =function () {
+  collectData();
+}
+function collectData() {
+  console.log ('form1');
+  return result;
+}
+
+
+
